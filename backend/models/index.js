723421ac4c99f1175fe46ee15adff709c2bb5f21
Larmier Anthony <larmieranthony@gmail.com>
2021-06-05 13:08:21 +0200

    commit cache fichier config

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,14 +2,14 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize( process.env.DB,  process.env.USER,  process.env.PASSWORD, {
   host: process.env.HOST,
-  dialect: process.env.dialect,
+  dialect: "mysql",
   operatorsAliases: false,
 
   pool: {
-    max: process.env.pool.max,
-    min: process.env.pool.min,
-    acquire: process.env.pool.acquire,
-    idle: process.env.pool.idle
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 
